726b9be7f6634852c037dd0055fb984e77fbbdb3
Fede Summo <federicosummo@outlook.com>
2022-09-28 15:30:30 -0300

    work in progress

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-export { default as Header } from "./Header";
-export { default as BackBtn } from "./BackBtn";
-export { default as PrimaryBtn } from "./PrimaryBtn";
-export { default as Input } from "./Input";
-export { default as SecondaryBtn } from "./SecondaryBtn";
 export { default as SessionHandler } from "./SessionHandler";
-export { default as ImgBtn } from "./ImgBtn";
 export { default as SafeAreaView } from "./SafeAreaView";
+export { default as SecondaryBtn } from "./SecondaryBtn";
+export { default as PrimaryBtn } from "./PrimaryBtn";
 export { default as Section } from "./Section";
+export { default as BackBtn } from "./BackBtn";
 export { default as LinkBtn } from "./LinkBtn";
+export { default as ImgBtn } from "./ImgBtn";
+export { default as Header } from "./Header";
+export { default as Input } from "./Input";
